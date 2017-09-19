3e13bce5ae38a662d4a3b49c2e7d729bd0974499
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-19 14:36:20 +0200

    creating user in test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe 'DB table' do
     it { is_expected.to have_db_column :name }
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :email }
   end
+
+  describe 'mailboxer' do
+    let (:sender) { User.create(email: 'sender@test.com', password: 'password', name: 'sender') }
+    let (:reciever) { User.create(email: 'receiver@test.com', password: 'password', name: 'receiver') }
+
+    it 'creates a user with valid info' do
+      expect(sender).to be_valid
+    end
+  end
+
 end
