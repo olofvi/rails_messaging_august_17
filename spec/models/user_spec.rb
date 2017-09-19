588ea5916b6b57c411a269fb31a85e4de8ac9a17
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-19 16:55:06 +0200

    added tests for sending messages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,11 +10,38 @@ RSpec.describe User, type: :model do
 
   describe 'mailboxer' do
     let (:sender) { User.create(email: 'sender@test.com', password: 'password', name: 'sender') }
-    let (:reciever) { User.create(email: 'receiver@test.com', password: 'password', name: 'receiver') }
+    let (:receiver) { User.create(email: 'receiver@test.com', password: 'password', name: 'receiver') }
 
     it 'creates a user with valid info' do
       expect(sender).to be_valid
     end
-  end
 
+    describe 'send message' do
+
+      before do
+        sender.send_message(receiver, "body", "subject")
+      end
+
+      it 'should be a new message in the recievers inbox' do
+        expect(receiver.mailbox.inbox.count).to eq 1
+      end
+
+      it 'should be a new message in the senders outbox' do
+        expect(sender.mailbox.sentbox.count).to eq 1
+      end
+
+      it 'should have a subject' do
+        expect(receiver.mailbox.inbox.last.subject).to eq "subject"
+      end
+
+      it 'should have a text body' do
+        conversation = receiver.mailbox.inbox.first
+        receipts = conversation.receipts_for receiver
+        receipts.each do |receipt|
+          @message = receipt.message
+        end
+        expect(@message.body).to eq "body"
+      end
+    end
+  end
 end
